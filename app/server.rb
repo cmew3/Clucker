e61daeda855a9b7da36b0a98efb0f885cdbfa778
cmew3 <c.wheeler-booth@cantab.net>
2014-08-05 20:55:53 +0100

    starting refactor

--- app/server.rb
+++ app/server.rb
@@ -30,6 +30,7 @@ get '/users/new' do
 end
 
 post '/users' do
+	puts "hello"
 	@user = User.new(name: params[:name],
 			email: params[:email],
 			user_name: params[:user_name],
@@ -129,7 +130,7 @@ end
 def send_password_reset(email, link)
   RestClient.post "https://api:key-d733bc88a2fb89d47ef91cfa2a5fb31a"\
   "@api.mailgun.net/v2/sandbox8e90015459c74825b6714ab541d9ac60.mailgun.org/messages",
-  :from => "Bookmark Manager <postmaster@sandbox8e90015459c74825b6714ab541d9ac60.mailgun.org>",
+  :from => "Clucker <postmaster@sandbox8e90015459c74825b6714ab541d9ac60.mailgun.org>",
   :to => "<#{email}>",
   :subject => "Password Reset",
   :text => "Please click the link below to reset your password\n#{link}."
